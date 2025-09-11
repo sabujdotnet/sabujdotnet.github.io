c84b111dff3799fc275b39f2e291f70fd1421a77
sabuj <168252321+sabujdotnet@users.noreply.github.com>
2025-09-11 19:46:56 +0600

    Add files via upload

---
+++ service-worker_Version2.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'formwork-pro-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  'shuttering-formwork-pro.html',
+  'manifest.json',
+  'assets/icons/icon-192.png',
+  'assets/icons/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
